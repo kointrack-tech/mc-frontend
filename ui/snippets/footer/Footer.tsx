a9281450690614d2b3074c57a178d93bd589afb5
Salesmask Techsystems <badrinath.mutkule@gmail.com>
2024-02-16 00:42:12 +0530

    fixed issue url

--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -18,8 +18,8 @@ import getApiVersionUrl from './utils/getApiVersionUrl';
 
 const MAX_LINKS_COLUMNS = 4;
 
-const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${ config.UI.footer.frontendVersion }`;
-const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${ config.UI.footer.frontendCommit }`;
+const FRONT_VERSION_URL = `https://github.com/metacypher/frontend/tree/${ config.UI.footer.frontendVersion }`;
+const FRONT_COMMIT_URL = `https://github.com/metacypher/frontend/commit/${ config.UI.footer.frontendCommit }`;
 
 const Footer = () => {
 
